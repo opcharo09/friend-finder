28143945ad6eaf8980dbde2ace91360e7db79761
unknown <oscar.charo09@gmail.com>
2019-05-24 20:56:25 -0500

    surver questions completed , updated answers in friends js

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,15 +4,15 @@ var users =[
     "name":"oscar",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        2,
+        5,
+        4,
+        3,
+        4,
+        5,
         1,
         2,
-        4,
         1,
         5,
-        2,
-        4,
-        4,
         5
       ]
   }];
