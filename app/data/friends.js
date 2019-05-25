aec3ccf8f1df1adb6af3a77e5baa97d1bf6a6cde
unknown <oscar.charo09@gmail.com>
2019-05-24 19:44:52 -0500

    home page

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var users =[
+
+{
+    "name":"oscar",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        2,
+        1,
+        2,
+        4,
+        1,
+        5,
+        2,
+        4,
+        4,
+        5
+      ]
+  }];
+
+  module.exports = users;
